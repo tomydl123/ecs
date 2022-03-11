09d1784e9aaeaa65fe289062fc6c5059486019fb
zhangjinggang <wow_20083683@163.com>
2022-03-11 11:23:43 +0800

    update

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,28 +1,47 @@
 module.exports = {
-    title: '大花花日记',
+    title: '大花花笔记',
     theme: '@vuepress/theme-blog',
     themeConfig: {
-      directories:[
-        {
-          id: 'post',
-          dirname: 'posts',
-          path: '/',
-          itemPermalink: '/post/:year-:month-:day/:slug',
-        }
-      ],
-      footer: {
-        contact: [
-          {
-            type: "mail",
-            link: "mailto:xxz_20083683@163.com"
-          }
+        nav: [
+            {
+                text: '笔记',
+                link: '/',
+            },
+            {
+                text: '标签',
+                link: '/tag/',
+            },
         ],
-        copyright: [
-          {
-            text: "京ICP备20020562号-1",
-            link: "http://www.beian.miit.gov.cn"
-          }
-        ]
-      },
+        directories: [
+            {
+                id: 'post',
+                dirname: 'posts',
+                path: '/',
+                itemPermalink: '/post/:year-:month-:day/:slug',
+            }
+        ],
+        globalPagination: {
+            prevText: '上一页',
+            nextText: '下一页',
+            lengthPerPage: 5
+        },
+        footer: {
+            contact: [
+                { type: 'github', link: 'https://github.com/tomydl123' },
+                {
+                    type: "mail",
+                    link: "mailto:xxz_20083683@163.com"
+                }
+            ],
+            copyright: [
+                {
+                    text: "京ICP备20020562号-1",
+                    link: "http://www.beian.miit.gov.cn"
+                }
+            ]
+        },
     },
-  }
+    markdown: {
+        lineNumbers: true
+    },
+}
